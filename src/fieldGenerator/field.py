3fc2b79c7feb2b7b64d6336c645bacb22838e6aa
leotalorac <leotalorac@unal.edu.co>
2020-05-25 17:19:28 -0500

    add divicion function

--- src/fieldGenerator/field.py
+++ src/fieldGenerator/field.py
@@ -43,26 +43,56 @@ def numberToBase(n, b,m):
         n //= b
     digits = digits + [0]*(m-len(digits))
     return digits[::-1]
+# taken from https://rosettacode.org/wiki/Polynomial_synthetic_division#Python
+def extended_synthetic_division(dividend, divisor):
+    '''Fast polynomial division by using Extended Synthetic Division. Also works with non-monic polynomials.'''
+    # dividend and divisor are both polynomials, which are here simply lists of coefficients. Eg: x^2 + 3x + 5 will be represented as [1, 3, 5]
+ 
+    out = list(dividend) # Copy the dividend
+    normalizer = divisor[0]
+    for i in range(len(dividend)-(len(divisor)-1)):
+        out[i] /= normalizer # for general polynomial division (when polynomials are non-monic),
+                                 # we need to normalize by dividing the coefficient with the divisor's first coefficient
+        coef = out[i]
+        if coef != 0: # useless to multiply if coef is 0
+            for j in range(1, len(divisor)): # in synthetic division, we always skip the first coefficient of the divisor,
+                                              # because it's only used to normalize the dividend coefficients
+                out[i + j] += -divisor[j] * coef
+ 
+    # The resulting out contains both the quotient and the remainder, the remainder being the size of the divisor (the remainder
+    # has necessarily the same degree as the divisor since it's what we couldn't divide from the dividend), so we compute the index
+    # where this separation is, and return the quotient and remainder.
+    separator = -(len(divisor)-1)
+    return out[:separator], out[separator:] # return quotient, remainder.
 
-def gf(p,n):
+# generate the fields
+def gf(p,n,pol):
+    pol = pol
+    zero = [0]*n
     nbig = p**n
     multiplications = list()
     sums = list()
     numbers = list()
     for i in range(nbig):
         numbers.append(numberToBase(i,p,n))
+    ii =0
     for i in numbers:
         ltem = []
         ltem2 = []
         for j in numbers:
             ltem.append(sum(i,j,p))
-            ltem2.append(mul(i,j,p))
-        print(ltem2)
+            m = mul(i,j,p)
+            mt = extended_synthetic_division(m,pol)[1]
+            mt = sum(mt,zero,p)
+            ltem2.append(mt)
+        if(ii==9):
+            print(ltem2)
+        ii=ii+1
         sums.append(ltem)
         multiplications.append(ltem2)
     return [multiplications,sums]
 
-gfs = gf(2,4)
+gfs = gf(2,4,[1,0,0,1,1])
 print("multiplications")
 print(gfs[0])
 print("sums")
