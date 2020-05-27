70582e624f6303de5d5863a1a40dd9a402c1a037
leotalorac <leotalorac@unal.edu.co>
2020-05-26 23:29:49 -0500

    poker test

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,3 +1,4 @@
+import math
 def frecuencia(n):
     b = bin(int(n,16))[2:]
     ones = b.count('1')
@@ -25,6 +26,35 @@ def serial(n):
     tr = (4/(n-1))*((zz**2)+(zo**2)+(oz**2)+(oo**2))
     tr = tr - (2/n)*(ones**2 + zeros**2)+1
     return tr
-def poker
+def findf(s,sf):
+    c =0
+    for i in range(0,len(s)-len(sf),len(sf)):
+        if(s[i:i+len(sf)] == sf):
+            c+=1
+    return c
+def poker(n):
+    bn = bin(int(n,16))[2:]
+    n = len(bn)
+    # get m
+    m =1
+    while (int(n/m) >= 5*(2**m)):
+        m+=1
+    m-=1
+    k = int(n/m)
+    # generate strings
+    s = []
+    for i in range(2**m):
+        st = bin(i)[2:]
+        s.append(str('0'*(m-len(st))+st))
+    f =[]
+    # get frecuency
+    for i in s:
+        f.append(findf(bn,i))
+    sf = 0
+    for i in f:
+        sf += i**2
+    pt = (((2**m)/k)*(sf))-k
+    return pt
+
 num = 'e3114ef249e3114ef249e3114ef249e3114ef249'
-print(serial(num))
+print(poker(num))
